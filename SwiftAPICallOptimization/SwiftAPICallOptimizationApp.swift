f7e14616013f8f66671c1225eda5b54db30952b3
matt-kay <kayodematthew56@gmail.com>
2023-05-10 09:15:27 +0100

    Initial Commit

---
+++ SwiftAPICallOptimization/SwiftAPICallOptimizationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftAPICallOptimizationApp.swift
+//  SwiftAPICallOptimization
+//
+//  Created by Mattosha on 10/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftAPICallOptimizationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
